7c3e965db006642493f28aac5e73ac63813a5d1a
julian <julian.chisholm@gmail.com>
2023-09-24 09:15:15 +0900

    check point 1 - blank screen with a centered ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
@@ -12,21 +13,31 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode (
             (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
     
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    print("Pygame Window was closed by the user")
-                    sys.exit()
+            self._check_events()
             
             # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
+            self._update_screen()
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
+    
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Pygame Window was closed by the user")
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()        
 
 if __name__ == '__main__':
     # Make a game instance and run the game
