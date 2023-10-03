b2b3d7f3de6aa42c588b9b2ec77d849e2b896251
julian <julian.chisholm@gmail.com>
2023-10-03 13:53:18 +0900

    Completed chapter 12

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
@@ -12,14 +13,25 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode (
             (self.settings.screen_width,self.settings.screen_height))
+        # uncomment the following for full screen mode
+        # self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
     
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             # Watch for keyboard and mouse events
             self._check_events()
+
+            # Update the ship
+            self.ship.update()
+
+            # Update the bullets
+            self._update_bullets()
             
             # Redraw the screen during each pass through the loop
             self._update_screen()
@@ -33,11 +45,55 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 print("Pygame Window was closed by the user")
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_event(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_event(event)
+
+    def _check_keydown_event(self,event):
+        """Respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            # Keep moving right until a KEYUP event
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            # Keep moving left until a KEYUP event
+            self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            # Fire a bullet
+            self._fire_bullet()
+        elif event.key == pygame.K_q:
+            # Quit the game if user hits 'q'
+            print("User hit 'q' to quit")
+            sys.exit()
+    
+    def _check_keyup_event(self,event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets"""
+        self.bullets.update()
+        # Get rid of bullets that have moved off the screen
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(f"len self.bullets = {len(self.bullets)}")
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()        
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+    
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets sprite group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 if __name__ == '__main__':
     # Make a game instance and run the game
