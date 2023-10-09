39ff5b15588e3f34e9047cb03defcc38dda69bc7
julian <julian.chisholm@gmail.com>
2023-10-09 18:58:36 +0900

    end of chapter 14

--- alien.py
+++ alien.py
@@ -13,7 +13,7 @@ class Alien(Sprite):
         # Load the alien image and set its rect attribute
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
-        print(f"alien rect = {self.rect}")
+        # print(f"alien rect = {self.rect}")
         
         # Start each new alien near the top left of the screen
         self.rect.x = self.rect.width
